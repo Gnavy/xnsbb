0c07752320c14919876cfab7ef2052a95905d4c5
luofengge <405963805@qq.com>
2016-08-25 18:13:42 +0800

    Clean Data import

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
+var clean = require('./routes/clean');
 var AV = require('leanengine');
-
-var clean = require('./routes/clean')
 
 var app = express();
 
@@ -32,14 +31,6 @@ app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() });
 });
 
-app.get('/clean', function(req, res) {
-  res.render('clean', { currentTime: new Date() });
-});
-
-app.get('/clean-detail', function(req, res) {
-  res.render('clean-detail', { currentTime: new Date() });
-});
-
 app.get('/phone-bound', function(req, res) {
   res.render('phone-bound', { currentTime: new Date() });
 });
@@ -49,6 +40,7 @@ app.get('/phone-verify', function(req, res) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+app.use("/clean",clean);
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
--- routes/clean.js
+++ routes/clean.js
@@ -1,3 +1,44 @@
 'use strict';
 var router = require('express').Router();
 var AV = require('leanengine');
+
+var CleanItem = AV.Object.extend("CleanItem");
+
+//clean首页
+router.get('/', function(req, res, next) {
+	var query = new AV.Query(CleanItem);
+	query.descending('ItemWT');
+	query.find().then(function(results) {
+		res.render('clean', {
+			items: results
+		});
+	}, function(err) {
+		if (err.code === 101) {
+			// 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
+			// 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
+			res.render('todos', {
+				title: 'clean',
+				todos: []
+			});
+		} else {
+			next(err);
+		}
+	}).catch(next);
+});
+//clean首页
+router.get('/detail/:itemId', function(req, res, next) {
+
+	var query = new AV.Query(CleanItem);
+	query.get(req.params.itemId).then(function(item) {
+		// 成功获得实例
+		// data 就是 id 为 57328ca079bc44005c2472d0 的 Todo 对象实例
+		res.render('clean-detail', {
+			item:item
+		});
+	}, function(error) {
+		// 失败了
+	});
+
+});
+
+module.exports = router;
