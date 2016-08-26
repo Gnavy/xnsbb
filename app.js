e0b99619108feae7ee4554102bf80d3d62c2fcd4
luofengge <405963805@qq.com>
2016-08-26 13:37:19 +0800

    替换图标

--- app.js
+++ app.js
@@ -24,53 +24,69 @@ require('./cloud');
 app.use(AV.express());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 
+var Swiper = AV.Object.extend("Swiper");
+
 app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
+
+	var query = new AV.Query(Swiper);
+	query.descending('SwiperWT');
+	query.find().then(function(results) {
+		res.render('index', {
+			swiper: results
+		});
+	}, function(err) {
+	});
 });
 
 app.get('/phone-bound', function(req, res) {
-  res.render('phone-bound', { currentTime: new Date() });
+	res.render('phone-bound', {
+		currentTime: new Date()
+	});
 });
 app.get('/phone-verify', function(req, res) {
-  res.render('phone-verify', { currentTime: new Date() });
+	res.render('phone-verify', {
+		currentTime: new Date()
+	});
 });
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
-app.use("/clean",clean);
+app.use("/clean", clean);
 
 app.use(function(req, res, next) {
-  // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
-  if (!res.headersSent) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  }
+	// 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
+	if (!res.headersSent) {
+		var err = new Error('Not Found');
+		err.status = 404;
+		next(err);
+	}
 });
 
 // error handlers
 app.use(function(err, req, res, next) { // jshint ignore:line
-  var statusCode = err.status || 500;
-  if(statusCode === 500) {
-    console.error(err.stack || err);
-  }
-  if(req.timedout) {
-    console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
-  }
-  res.status(statusCode);
-  // 默认不输出异常详情
-  var error = {}
-  if (app.get('env') === 'development') {
-    // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
-    error = err;
-  }
-  res.render('error', {
-    message: err.message,
-    error: error
-  });
+	var statusCode = err.status || 500;
+	if (statusCode === 500) {
+		console.error(err.stack || err);
+	}
+	if (req.timedout) {
+		console.error('请求超时: url=%s, timeout=%d, 请确认方法执行耗时很长，或没有正确的 response 回调。', req.originalUrl, err.timeout);
+	}
+	res.status(statusCode);
+	// 默认不输出异常详情
+	var error = {}
+	if (app.get('env') === 'development') {
+		// 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
+		error = err;
+	}
+	res.render('error', {
+		message: err.message,
+		error: error
+	});
 });
 
 module.exports = app;
--- public/js/common.js
+++ public/js/common.js
@@ -8,3 +8,7 @@ function checkMobile(phoneNumber) {
 	}
 
 }
+function escape2Html(str) {
+ var arrEntities={'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"'};
+ return str.replace(/&(lt|gt|nbsp|amp|quot);/ig,function(all,t){return arrEntities[t];});
+}
